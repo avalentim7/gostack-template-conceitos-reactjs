5d6c1a237d9eac0540869161bfd6696e366eb2f6
engageavalentim <54715725+engageavalentim@users.noreply.github.com>
2020-12-01 21:09:11 -0300

    desafio: criar listagem, cadastro e remoção de repositorios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories')
+      .then(response => {
+        setRepositories(response.data);
+      })
+      .catch(err => console.warning(err));
+
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Front-end com ReactJS ${Date.now()}`,
+      url: 'https://github.com/avalentim7',
+      techs: ['NodeJS', 'ReactJS']
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    const repositoriesRemaining = repositories.filter(repository => repository.id !== id);
+    
+    setRepositories(repositoriesRemaining);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}> Remover </button>
+            </li>
+          )
+        })
+        }
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
